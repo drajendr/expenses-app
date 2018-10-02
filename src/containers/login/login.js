2d4977f8198db2720be28a1f5e00c414352edca7
Dinesh Rajendran <dineshdinesh052@gmail.com>
2018-10-02 16:54:13 +0530

    Added material ui

---
+++ src/containers/login/login.js
@@ -0,0 +1,11 @@
+import React, { component } from 'react';
+
+class Login extends component{
+    render(){
+        return{
+            
+        }
+    }
+}
+
+export default Login;
---
+++ src/containers/register/register.js
@@ -0,0 +1,16 @@
+import React, { component } from 'react';
+import Aux from '../../hoc/common/aux';
+
+class Register extends component{
+    render(){
+        return(
+            <Aux>
+                <form>
+                    
+                </form>
+            </Aux>
+        )                    
+    }
+}
+
+export default Register;
